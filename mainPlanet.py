a31bc222c2fb56864053fe499cbdd16b6a16e635
toskey <augh>
2023-08-30 21:40:14 +0200

    hehehe

--- mainPlanet.py
+++ mainPlanet.py
@@ -1,1 +1,8 @@
 import numpy as np
+class Planet:
+    def __init__(self, masse = 1, size = 1, x = 100, y = 0):
+        self.masse = masse
+        self.size = size
+        self.x = x
+        self.y = y
+print (Planet().x)
